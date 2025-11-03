1a848c5e4f798ab0df59932d0f83c9af717089d8
tasueom <tasueom@naver.com>
2025-11-03 16:14:03 +0900

    feat: 분석 성공과 실패에 따른 플래시 메시지 출력

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ def upload():
     required_columns = ['대여소_ID', '주소1', '주소2', '위도', '경도']
     
     if list(df.columns) != required_columns:
+        flash('필수 칼럼명이 일치하지 않습니다.')
         return redirect(url_for('index'))
     
     df = df['주소1']
@@ -34,6 +35,7 @@ def upload():
         "value": value_counts.values.tolist()
     }
     
+    flash('대여소 분포 분석이 완료되었습니다.')
     return render_template('index.html', gu_data=gu_data)
 
 if __name__ == '__main__':
